674324a25196c60b8cde52ecfae20537062f20b0
asadmughal786 <106271884+asadmughal786@users.noreply.github.com>
2022-09-27 12:35:13 +0500

    Done the login

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User_signup
 
 # Register your models here.
+
+@admin.register(User_signup)
+class Users(admin.ModelAdmin):
+    list_display = ['id','user_fname','user_lname','user_cnic', 'user_email','user_contact', 'user_address','user_password','user_conf_password']
+    list_display_links = ['id', 'user_email', 'user_contact']
+    readonly_fields = ('user_email','user_contact')
--- users/views.py
+++ users/views.py
@@ -16,7 +16,7 @@ def log_in(request):
         Conf_passsword = request.POST.get('confPassword')
         User_email = request.POST.get('email')
         User_address = request.POST.get('Address')
-        print(f'First Name: {First_name},Last Name: {Last_name},CNIC: {CNIC}, Email: {User_email}')
+        print(f'First Name: {list(First_name)[0]},Last Name: {Last_name},CNIC: {CNIC}, Email: {User_email}')
         user_data = User_signup(user_fname = First_name , user_lname = Last_name, user_contact = Contact_number, user_cnic = CNIC ,user_email=User_email,user_address = User_address, user_password= Pass_word,user_conf_password=Conf_passsword)
         user_data.save()        
         return redirect("login")
