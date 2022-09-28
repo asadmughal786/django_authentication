6e50f9831901f92c9e8c5c26864701da0c8ad577
asadmughal786 <106271884+asadmughal786@users.noreply.github.com>
2022-09-28 13:25:03 +0500

    messages and authentication implemented

--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
-from http.client import ImproperConnectionState
+import email
+from email import message
+from django.contrib import messages
+from pyexpat.errors import messages
 from django.shortcuts import render,redirect
 from .models import User_signup
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
@@ -17,9 +20,19 @@ def log_in(request):
         User_email = request.POST.get('email')
         User_address = request.POST.get('Address')
         print(f'First Name: {list(First_name)[0]},Last Name: {Last_name},CNIC: {CNIC}, Email: {User_email}')
-        user_data = User_signup(user_fname = First_name , user_lname = Last_name, user_contact = Contact_number, user_cnic = CNIC ,user_email=User_email,user_address = User_address, user_password= Pass_word,user_conf_password=Conf_passsword)
-        user_data.save()        
-        return redirect("login")
+        user_data = User_signup.objects.filter(user_email = User_email)
+        if user_data:
+            messages.info(request,"Email Already Exsists")
+            return redirect('log_in')
+        if Pass_word == Conf_passsword:
+            user_data = User_signup(user_fname = First_name , user_lname = Last_name, user_contact = Contact_number, user_cnic = CNIC ,user_email= User_email,user_address = User_address, user_password= Pass_word,user_conf_password=Conf_passsword)
+            # user_data.set_password(Pass_word)
+            user_data.save()
+            messages.info(request,"User saved successfully")        
+            return redirect("log_in")
+        else:
+            messages.warning(request,'Passwords does not match')
+            return redirect("log_in")
     return render(request,'users/login.html')
 
 
